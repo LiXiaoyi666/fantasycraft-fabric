cca9bdd8f1f904f62f18eb0e2b48f7942e5b1888
LiXiaoyi <KevinHelmar@163.com>
2024-06-29 15:11:00 +0800

    Initial commit

---
+++ src/main/java/net/fantasycraft/FantasyCraft.java
@@ -0,0 +1,23 @@
+package net.fantasycraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FantasyCraft implements ModInitializer {
+	// 该日志记录器用于将文本写入控制台和日志文件。
+	// 最佳做法是使用您的模式 ID 作为记录器名称。
+	// 这样，信息、警告和错误就会一目了然。
+	public static final String MOD_ID = "fantasycraft";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// 一旦 Minecraft 处于可加载 MOD 的状态，这段代码就会立即运行。
+		// 不过，有些东西（如资源）可能仍未初始化。
+		// 谨慎行事。
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/fantasycraft/FantasyCraftClient.java
@@ -0,0 +1,10 @@
+package net.fantasycraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FantasyCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
